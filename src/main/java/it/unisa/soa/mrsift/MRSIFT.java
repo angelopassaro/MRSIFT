3989c0f414ea073dffe48e2cbf7332ec044493fa
AddeusExMachina <antaddnf@gmail.com>
2018-07-12 20:28:46 +0200

    add Tool interface

--- src/main/java/it/unisa/soa/mrsift/MRSIFT.java
+++ src/main/java/it/unisa/soa/mrsift/MRSIFT.java
@@ -1,16 +1,22 @@
 package it.unisa.soa.mrsift;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.Set;
+import javax.lang.model.SourceVersion;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.MapWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 /**
  *
  * @author Diego Avella
@@ -18,7 +24,7 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
  * @author Antonio Addeo
  * This class is the main M/R Job configuration
  */
-public class MRSIFT {
+public class MRSIFT extends Configured implements Tool {
 
   private static final String OPENCV_LIB = "/libs/libopencv_java341.so";
 
@@ -28,21 +34,37 @@ public class MRSIFT {
     System.load(opencv.getAbsolutePath());
   }
 
-  public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException, URISyntaxException {
-    Configuration conf = new Configuration();
+  public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException, 
+URISyntaxException, Exception {
+      int res = ToolRunner.run(new Configuration(), new MRSIFT(), args);
+      System.exit(res);
+  }
+
+    @Override
+    public int run(String[] strings) throws Exception {
+    Configuration conf = this.getConf();
+    
     Job job = Job.getInstance(conf, "MRSIFT");
+    
     job.addFileToClassPath(new Path(MRSIFT.class.getResource(OPENCV_LIB).toURI()));
-    job.setJarByClass(MRSIFT.class);
+    job.setJarByClass(MRSIFT.class);    
     job.setMapperClass(SiftMapper.class);
+    job.setCombinerClass(SiftReduce.class);
     job.setReducerClass(SiftReduce.class);
     job.setMapOutputKeyClass(Text.class);
     job.setMapOutputValueClass(MapWritable.class);
     job.setInputFormatClass(ImageInputFormat.class);
     job.setOutputFormatClass(ImageOutputFormat.class);
-    job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(MatWritable.class);
-    FileOutputFormat.setOutputPath(job, new Path(args[1]));
-    FileInputFormat.addInputPath(job, new Path(args[0]));
-    System.exit(job.waitForCompletion(true) ? 0 : 1);
-  }
+    
+    ImageOutputFormat.setOutputPath(job, new Path(strings[1]));
+    ImageInputFormat.addInputPath(job, new Path(strings[0]));
+    
+    return job.waitForCompletion(true) ? 0 : 1;       
+    }
+
+    public Set<SourceVersion> getSourceVersions() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
+
